ea9abc8b886b36d568c2d22f55eaab33a7748ff0
Sebastian Kärner <sebastiankarner@live.se>
2023-06-30 14:11:49 +0200

    ADD - Tried to update userentity ridearray on new ride add but im unsucessful, will try again later.

--- src/main/java/com/example/mongodbtestprogram/Dto/BikeDTO.java
+++ src/main/java/com/example/mongodbtestprogram/Dto/BikeDTO.java
@@ -3,19 +3,21 @@ package com.example.mongodbtestprogram.Dto;
 import com.example.mongodbtestprogram.Enum.Sizes;
 import lombok.Value;
 
+import java.util.List;
 import java.util.UUID;
 
 @Value
 public class BikeDTO {
 
     UUID bikeId;
+
     String maker;
     String model;
     Sizes size;
-    String[] pictures;
+    List<String> pictures;
     Long year;
     String type;
-    String[] colors;
+    List<String> colors;
     String material;
     Double wheelSize;
     String gears;
