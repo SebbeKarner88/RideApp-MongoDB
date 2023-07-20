9f8d6821c7ca252675d63833b7f4eb33f2ada5ce
Sebastian Kärner <sebastiankarner@live.se>
2023-07-20 09:38:27 +0200

    UPDATE - Added a frontend start with react Vite

--- src/main/java/com/example/mongodbtestprogram/Service/RideService.java
+++ src/main/java/com/example/mongodbtestprogram/Service/RideService.java
@@ -13,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class RideService {
