38e47d17407654f11579c66f37ab4ca75a34d061
SebastianKärner88 <sebastiankarner@live.se>
2024-04-09 12:22:22 +0200

    Revert "changes to headers"
    This reverts commit cf6b15454f610c8fc781f9424261bd397bbed535.

--- Frontend/src/services/fetch.api.tsx
+++ Frontend/src/services/fetch.api.tsx
@@ -1,17 +1,17 @@
+
 import {FieldValues} from "react-hook-form";
 import {IGeoLocation} from "../interfaces/IGeoLocation.ts";
 import {IRide} from "../interfaces/IRide.ts";
 import {IBike} from "../interfaces/IBike.ts";
 
 
-export const fetchApi = {
+ export const fetchApi = {
 
     login: (login: FieldValues) => {
         return (
             fetch("https://rideapp-latest.onrender.com/api/auth/authenticate", {
                 method: "POST",
                 headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(login),
@@ -21,133 +21,125 @@ export const fetchApi = {
         );
     },
 
-    register: (data: FieldValues) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/api/auth/register", {
-                method: "POST",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     register: (data: FieldValues) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/api/auth/register", {
+                 method: "POST",
+                 headers: {
+                     "Content-Type": "application/json",
+                 },
+                 body: JSON.stringify(data),
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    getAllBikes: (auth: string) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/bike/getAll", {
-                method: "GET",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     getAllBikes: (auth: string) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/bike/getAll", {
+                 method: "GET",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "Content-Type": "application/json",
+                 },
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    getAllRides: (userId: string, auth: string) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/rides/getAllByUserId", {
-                method: "GET",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "userId": userId,
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     getAllRides: (userId: string, auth: string) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/rides/getAllByUserId", {
+                 method: "GET",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "userId": userId,
+                     "Content-Type": "application/json",
+                 },
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    getBikeCollectionByUserId: (userId: string, auth: string) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/user/getBikeCollectionByUserId", {
-                method: "GET",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "userId": userId,
-                    "Content-Type": "application/json",
-                },
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     getBikeCollectionByUserId: (userId: string, auth: string) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/user/getBikeCollectionByUserId", {
+                 method: "GET",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "userId": userId,
+                     "Content-Type": "application/json",
+                 },
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    startNewRide: (userId: string, bikeId: string, auth: string, rideEntity: IRide) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/rides/add", {
-                method: "POST",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "userId": userId,
-                    "bikeId": bikeId,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(rideEntity),
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     startNewRide: (userId: string, bikeId: string, auth: string, rideEntity:IRide) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/rides/add", {
+                 method: "POST",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "userId": userId,
+                     "bikeId": bikeId,
+                     "Content-Type": "application/json",
+                 },
+                 body: JSON.stringify(rideEntity),
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    addGeoLocCheckpoint: (auth: string, rideId: string, geoLoc: IGeoLocation[]) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/rides/addCheckpoint", {
-                method: "POST",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "rideId": rideId,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(geoLoc),
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     addGeoLocCheckpoint: (auth: string, rideId: string, geoLoc: IGeoLocation[]) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/rides/addCheckpoint", {
+                 method: "POST",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "rideId": rideId,
+                     "Content-Type": "application/json",
+                 },
+                 body: JSON.stringify(geoLoc),
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    addBikeToCollection: (userId: string, auth: string, bike: IBike) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/bike/addToBikeCollection", {
-                method: "POST",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "userId": userId,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(bike),
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     addBikeToCollection: (userId: string, auth: string, bike:IBike) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/bike/addToBikeCollection", {
+                 method: "POST",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "userId": userId,
+                     "Content-Type": "application/json",
+                 },
+                 body: JSON.stringify(bike),
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 
-    addNewBike: (auth: string, bike: IBike) => {
-        return (
-            fetch("https://rideapp-latest.onrender.com/bike/add", {
-                method: "POST",
-                headers: {
-                    "Access-Control-Allow-Origin": "https://ride-app-mongo-db-5qft.vercel.app/",
-                    "Authorization": "Bearer " + auth,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(bike),
-            })
-                .then((response) => response.json())
-                .catch((e) => console.log(`Could not fetch data ${e}`))
-        );
-    },
+     addNewBike: (auth: string, bike:IBike) => {
+         return (
+             fetch("https://rideapp-latest.onrender.com/bike/add", {
+                 method: "POST",
+                 headers: {
+                     "Authorization": "Bearer " + auth,
+                     "Content-Type": "application/json",
+                 },
+                 body: JSON.stringify(bike),
+             })
+                 .then((response) => response.json())
+                 .catch((e) => console.log(`Could not fetch data ${e}`))
+         );
+     },
 }
